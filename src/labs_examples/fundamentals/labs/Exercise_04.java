562c708c19fb27c019ecc8112a603e101650f12c
deltafiveoh <75449438+deltafiveoh@users.noreply.github.com>
2020-12-04 10:42:30 -0600

    exercises 4 and 5  Please enter the commit message for your changes. Lines starting

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,6 +16,33 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        // char example
+        char c = 'c';
+        System.out.println("char c is: " + c);
+
+        // short example
+        short s = 2333;
+        System.out.println("short s is: " + s);
+
+        // byte example
+        byte b = 1;
+        System.out.println("byte b is: " + b);
+
+        // long example
+        long l = 1234883838838l;
+        System.out.println("long l is: " + l);
+
+        // boolean example
+        boolean what = false;
+        System.out.println("boolean what is: " + what);
+
+        // float example
+        float f = 12.1f;
+        System.out.println("float f is: " + f);
+
+        // double example
+        double d = 12.12323;
+        System.out.println("double d is: " + d);
 
     }
 
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,14 +14,32 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int x = str.length();
+        System.out.println("hello! length = " + x);
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean Strequals = str.equals(str2);
+        System.out.println("str is equal to str2?: " + Strequals);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String str3 = str + str2;
+        System.out.println("str3 is: " + str3);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        String substr = str.substring(0,4);
+        String replaces = str.replaceAll(str, "hi");
+        String newstring = str.concat(str2);
+
+        System.out.println(substr + ", " + replaces + ", " + str + ", " + str.codePointAt(3));
+
+        System.out.println(newstring + ", " + str.contains("e"));
+
+        String example = "whatever you want";
+        String example2 = example.replaceAll("\\s","");
+
+        System.out.println(example2);
 
     }
 
