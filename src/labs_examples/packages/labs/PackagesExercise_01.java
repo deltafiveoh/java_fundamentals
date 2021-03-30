15a1e483e4ab3408d7af5a29e2b9975d4ed4f493
deltafiveoh <75449438+deltafiveoh@users.noreply.github.com>
2021-03-30 14:39:33 -0500

    packages

--- src/labs_examples/packages/labs/PackagesExercise_01.java
+++ src/labs_examples/packages/labs/PackagesExercise_01.java
@@ -9,3 +9,8 @@ package labs_examples.packages.labs;
  *      4) Demonstrate how the the protected access modifier restricts access from package to package
  *
  */
+
+// Public is open to all packages within the program. Private variables and methods are only open to methods within the same class. This is why we often use
+// public getters and setters. The variables are private to the class (encapsulated) and can not be accessed outside the class. Protected implies only that classes within the same package
+// and sub-classes (children) within the package or children within another package that inherit from the protected parent can access the variables or methods inside. Default takes away the
+// ability of sub-classes outside the package to modify.
